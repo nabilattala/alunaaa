7cfad5b39a57231be0699a9527e507a87d98cf2a
MRCOOL222 <fatihmuttaqein211@gmail.com>
2025-04-13 21:03:09 +0700

    Fix active user

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,7 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'role' => $this->role,
+            'is_active' => $this->is_active,
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
