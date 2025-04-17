bc35ef305a6e5e4fb3508d41d35cc6c8dfb2ded1
ItsMeIfadh <itsmeifadh@gmail.com>
2025-04-17 10:40:21 +0700

    url photo user

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -9,18 +9,20 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'            => $this->id,
-            'username'      => $this->username,
-            'email'         => $this->email,
-            'role'          => $this->role,
-            'phone_number'  => $this->phone_number,
-            'is_active'     => $this->is_active,
-            'address'       => $this->address,
-            'profile_photo' => $this->profile_photo 
-                                ? url('uploads/profile_photos/' . $this->profile_photo) 
-                                : null,
-            'created_at'    => $this->created_at,
-            'updated_at'    => $this->updated_at,
+            'id' => $this->id,
+            'username' => $this->username,
+            'email' => $this->email,
+            'role' => $this->role,
+            'phone_number' => $this->phone_number,
+            'address' => $this->address,
+            'is_active' => $this->is_active,
+            'profile_photo_url' => $this->profile_photo
+                ? (str_starts_with($this->profile_photo, 'http') 
+                ? $this->profile_photo 
+                : url('uploads/profile_photos/' . $this->profile_photo))
+                : null,
         ];
     }
+
+
 }
